206f264c066e13042dbd35db8ea6a80c415dd09b
aashishChakradhar <ashishchakradhar01@gmail.com>
2024-06-30 17:15:52 +0545

    control flow in js

--- learnbasics/myscript.js
+++ learnbasics/myscript.js
@@ -1,4 +1,10 @@
-var weightPound = prompt("Enter your wight in pound: ")
-var weightKg = weightPound * 0.454
-alert("your weight in Kg is:  "+ weightKg)
-console.log("Conversion Completed")
+var guessNum = prompt('Enter the number: ')
+if(guessNum > 10){
+    alert("Very far")
+}
+else if(guessNum > 5 && guessNum < 10){
+    alert("near")
+}
+else{ 
+    alert("tTrue num")
+} 
