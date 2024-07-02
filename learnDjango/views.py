350aad123c8ca0e19db95727d57906e55d7a113e
aashishChakradhar <ashishchakradhar01@gmail.com>
2024-07-02 14:20:05 +0545

    normal setup

--- learnDjango/views.py
+++ learnDjango/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse
 
 # Create your views here.
+def index(response):
+    response = """<html>
+    <head></head>
+    <body>
+    <h1>hello</h1>
+    </body>
+    </html>"""
+    return HttpResponse(response)
