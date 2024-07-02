aede72596a62793845396612cfc52d5fb365851b
aashishChakradhar <ashishchakradhar01@gmail.com>
2024-07-02 16:50:11 +0545

    learning about django inbuilt forms and class view approach

--- learnDjango/urls.py
+++ learnDjango/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from learnDjango import views
 urlpatterns = [
     path('', views.Normal.as_view(), name='index'),
+    path('index', views.Normal.as_view(), name='index'),
+    path('auth', views.AuthView.as_view(), name='auth'),
 ]
