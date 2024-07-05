8939bc5899ab449e8ceaa17aacdc28b79a1210df
aashishChakradhar <ashishchakradhar01@gmail.com>
2024-07-05 20:06:07 +0545

    learning gradient descent for linear regression

--- machineLearning/linearRegression.py
+++ machineLearning/linearRegression.py
@@ -2,22 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 print(f"Learning about linear regression")
 
-def func(w,x,b):
-# model function
-    fun = w*x +b
-    return(fun)
+def model_function(x,w,b):
+    # model function
+    y_predict = w*x +b
+    return(y_predict)
 
-def output_model(x,w,b):
-# produce prediction for y
-    m = len(x)
-    f_wb = np.zeros(m) # produces array of m numbers with 0 as its elements
+def model_function_array(x_train,w,b):
+    # produce prediction for y
+    m = len(x_train)
+    y_predict = np.zeros(m) # produces array of m numbers with 0 as its elements
     for i in range(m):
-        f_wb[i]=func(w,x[i],b)
-    return f_wb
+        y_predict[i]=model_function(w,x_train[i],b)
+    return y_predict #  returns array
+
+def cost_function(x_train,y_train,w,b):
+    m = len(x_train)
+    y_predict = model_function_array(x_train,w,b)
+    error = 0
+    for i in range(0,m):
+        error += (y_predict[i]-y_train[i])**2
+    error = ( 1/(2*m))*error
+    return error
+
+
 
 # training data set
-x_train =  np.array([1,2])
-y_train = np.array([300,500])
+x_train =  np.array([1,2,3])
+y_train = np.array([300,500,700])
 m = len(x_train)
 print(f"x_train: {x_train}\ny_train: {y_train}\nthe number of training data set(m): {m}")
 
@@ -26,21 +37,24 @@ plt.title("housing price")
 plt.ylabel('price in 1000')
 plt.xlabel('size in 1000sqft')
 
-# to plot the data set
+# to plot the training data set
 w= 200
 b = 100
-plt.scatter(x_train,y_train,marker = 'x', c='r',label="Actual values")
+plt.scatter(x_train,y_train,marker = 'x', c='y',label="Actual values")
 # scatter gives the points only
 
-#to test model function
-temp = output_model(x_train,w,b)
-plt.plot(x_train,temp, c='b',label = "Predicted values")
+#to test model function that we created
+y_predict = model_function_array(x_train,w,b)
+plt.plot(x_train,y_predict,marker='x', c='b',label = "Predicted values")
 #plot gives the solid line joining the points
 
 # to make prediction
 x = float(input("Enter square feet: "))
-cost = func(w,x,b)
-plt.scatter(x,cost,marker='o',c='g',label='Specified Price')
-print(f"the cost of the house is: {cost}")
+y1 = model_function(x,w,b)
+plt.scatter(x,y1,marker='o',c='g',label='Specified Price')
+print(f"the cost of the house is: {y1}")
+error = cost_function(x_train,y_train,w,b)
+print(f"Error is: {error}")
+plt.scatter(x,error,marker='o',c='r',label='Error')
 plt.legend()
 plt.show()
