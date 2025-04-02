247d5dc741cf36c7f4ed6923af3e5595691e8b8d
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-04-02 13:35:52 +0545

    complete drum keypress task

--- roadToReact/drum/scripts.js
+++ roadToReact/drum/scripts.js
@@ -1,7 +1,58 @@
+let buttonList = ['a','s','d','f','j','k','l'];
+// button press through mouse
 let keys = document.querySelectorAll(".drum-button");
-var sounds = ["crash.mp3","kick-bass.mp3","snare.mp3","tom-1.mp3","tom-2.mp3","tom-3.mp3","tom-4.mp3"];
 for (let index = 0; index < keys.length; index++) {
-    keys[index].addEventListener("click",function(){
-        new Audio(`sounds\\${sounds[index]}`).play(); 
-    });    
+    keys[index].addEventListener("click", function () {
+        var key = this.innerHTML.trim();
+        if (buttonList.includes(key)){
+            makeSound(key);
+            highlight(key);
+        }
+        
+    });
+}
+
+// keyboard press
+document.addEventListener('keypress', function(event){
+    if(buttonList.includes(event.key)){
+        makeSound(event.key);
+        highlight(event.key);
+    }
+});
+
+function highlight(key){
+    document.querySelector("."+key+"-button").classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("."+key+"-button").classList.remove("pressed");
+    },200);
+}
+
+// for playing sound
+function makeSound(key){
+    switch (key) {
+        case "a":
+            new Audio('sounds/crash.mp3').play();   
+            break;
+        case 's':
+            new Audio('sounds/kick-bass.mp3').play();
+            break;
+        case 'd':
+            new Audio('sounds/snare.mp3').play();
+            break;
+        case 'f':
+            new Audio('sounds/tom-1.mp3').play();
+            break;
+        case 'j':
+            new Audio('sounds/tom-2.mp3').play();
+            break;
+        case 'k':
+            new Audio('sounds/tom-3.mp3').play();
+            break;
+        case 'l':
+            new Audio('sounds/tom-4.mp3').play();
+            break;          
+        default:
+            alert("Press the Assigned button");
+            break;
+    }
 }
